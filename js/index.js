d165b0aac0c73dd86139eeab6afa1b8a15a11779
alessandro-toscano <60431526+alessandro-toscano@users.noreply.github.com>
2020-01-29 14:13:38 +0100

    Add files via upload
    Initial commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+  // BACK TO TOP BUTTON
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 400) { // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(500); // Fade in the arrow
+    } else {
+      $('#return-to-top').fadeOut(500); // Else fade out the arrow
+    }
+  });
+
+  $('#return-to-top').click(function() { // When arrow is clicked
+    $('body,html').animate({
+      scrollTop: 0 // Scroll to top of body
+    }, 1000);
+  });
+
+
+  // ISOTOPE INIT
+  $('.grid').isotope({
+    itemSelector: '.grid-item',
+  });
+
+  // filter items on button click
+  $('.filter-button-group').on('click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $('.grid').isotope({ filter: filterValue });
+    $('.filter-button-group li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+
+  // CUSTOM GALLERY
+  $(".item_content").on("click", function() {
+    $(".backDrop").animate({ "opacity": ".70" }, 500);
+    $(".box").animate({ "opacity": "1.0" }, 500);
+    $(".backDrop, .box").css("display", "block");
+    var largeImage = $(this).find('img').attr("src");
+    $(".largeImage").attr({ src: largeImage });
+  });
+
+  $(".close, .backDrop").on("click", function() {
+    closeBox();
+  });
+
+  function closeBox() {
+    $(".backDrop, .box").animate({ "opacity": "0" }, 500, function() {
+      $(".backDrop, .box").css("display", "none");
+    });
+  }
+
+});
